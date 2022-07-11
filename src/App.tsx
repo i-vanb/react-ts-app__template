21c1a58abd726cfb4daa26e3eb586274f3fcc0ae
Ivan B <ivan.giv14@gmail.com>
2022-07-11 19:35:28 +0300

    example element in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
 export const App = () => {
-    return <div></div>
+    return (
+        <div>
+            {/*<Example title='' />*/}
+        </div>
+    )
 }
+
+type ExampleProps = {
+    title:string
+}
+
+const Example = (props:ExampleProps) => {
+    return (
+        <div style={{display:'flex', alignItems:'center',justifyContent:'center', flexDirection:"column"}}>
+            <h1>{props.title}</h1>
+
+        </div>
+    )
+}
+
